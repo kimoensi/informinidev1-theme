31f0bc0621f9369b5de7c7174a992d174802856d
kamel Malleh <kamel.mellah@gmail.com>
2018-05-18 15:55:47 +0100

    Add files via upload

---
+++ post_install.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+# This file is executed in the Rails evironment by the `rails-post-install` script
+
+def column_exists?(table, column)
+  if table_exists?(table)
+    ActiveRecord::Base.connection.column_exists?(table, column)
+  end
+end
+
+# Add the user_address field to the User model
+unless column_exists?(:users, :user_address)
+  migration_file_path = '../db/migrate/kamel_theme_add_user_address_to_user'
+  require File.expand_path migration_file_path, __FILE__
+  KamelThemeAddUserAddressToUser.up
+end
+
+# Create any necessary global Censor rules
+require File.expand_path(File.dirname(__FILE__) + '/lib/censor_rules')
---
+++ uninstall.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+# Uninstall hook code here
+
+def column_exists?(table, column)
+  if table_exists?(table)
+    ActiveRecord::Base.connection.column_exists?(table, column)
+  end
+end
+
+if ENV['REMOVE_MIGRATIONS']
+  # Remove the identity_card_number field to the User model
+  if column_exists?(:users, :user_address)
+    migration_file_path = '../db/migrate/kamel_theme_add_user_address_to_user'
+    require File.expand_path migration_file_path, __FILE__
+    KamelThemeAddUserAddressToUser.down
+  end
+end
