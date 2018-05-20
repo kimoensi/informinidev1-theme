69264e46061c22ac25064ef1193d695dae4fa4d5
kamel Malleh <kamel.mellah@gmail.com>
2018-05-20 14:03:50 +0100

    Update kamel_theme_add_user_address_to_user.rb

--- db/migrate/kamel_theme_add_user_address_to_user.rb
+++ db/migrate/kamel_theme_add_user_address_to_user.rb
@@ -1,10 +1,20 @@
 # -*- encoding : utf-8 -*-
-class KamelThemeAddUserAddressToUser < ActiveRecord::Migration
+class Informinidev1ThemeAddUserAddressToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :user_address, :string
   end
-
+  
   def self.down
     remove_column :users, :user_address
+  end
+end
+
+class Informinidev1ThemeAddUserStatusToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :user_status, :boolean
+  end
+  
+  def self.down
+    remove_column :users, :user_status
   end
 end
