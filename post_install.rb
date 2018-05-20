ccbd6384cd2bc937739d03f0f2bef411e43700e9
kamel Malleh <kamel.mellah@gmail.com>
2018-05-20 14:00:13 +0100

    Update post_install.rb

--- post_install.rb
+++ post_install.rb
@@ -10,9 +10,16 @@ def column_exists?(table, column)
   end
 end
 
+# Add the user_status field to the User model
+unless column_exists?(:users, :user_status)
+  migration_file_path = '../db/migrate/informinidev1_theme_add_user_status_to_user'
+  require File.expand_path migration_file_path, __FILE__
+  Informinidev1ThemeAddUserStatusToUser.up
+end
+
 # Add the user_address field to the User model
 unless column_exists?(:users, :user_address)
-  migration_file_path = '../db/migrate/kamel_theme_add_user_address_to_user'
+  migration_file_path = '../db/migrate/Informinidev1_theme_add_user_address_to_user'
   require File.expand_path migration_file_path, __FILE__
-  KamelThemeAddUserAddressToUser.up
+  Informinidev1ThemeAddUserAddressToUser.up
 end
