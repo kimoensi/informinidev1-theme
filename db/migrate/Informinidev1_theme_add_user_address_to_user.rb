21d55030b3a15d5adf901ad79ad8694d757041f8
kamel Malleh <kamel.mellah@gmail.com>
2018-06-03 08:51:48 +0100

    Update Informinidev1_theme_add_user_address_to_user.rb

--- db/migrate/Informinidev1_theme_add_user_address_to_user.rb
+++ db/migrate/Informinidev1_theme_add_user_address_to_user.rb
@@ -1,1 +1,10 @@
-
+# -*- encoding : utf-8 -*-
+class Informinidev1ThemeAddUserAddressToUser < ActiveRecord::Migration
+  def self.up
+    add_column :users, :user_address, :string
+  end
+  
+  def self.down
+    remove_column :users, :user_address
+  end
+end
