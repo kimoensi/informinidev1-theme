555d2e6d1df183c7c68e760551d2cb73014e659d
kamel Malleh <kamel.mellah@gmail.com>
2018-06-03 12:06:34 +0100

    Update controller_patches.rb

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -6,22 +6,19 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-
-
-
+    
+    
     UserController.class_eval do
-
-
-      private
-
-      def user_params(key = :user)
-          params[key].slice(:name,
-                            :email,
-                            :user_address,
-                            :user_status,  
-                            :password,
-                            :password_confirmation
-      end
+        private
+        
+        def user_params(key = :user)
+            params[key].slice(:name,
+                              :email,
+                              :user_address,
+                              :user_status,  
+                              :password,
+                              :password_confirmation
+        end
 
     end
   
